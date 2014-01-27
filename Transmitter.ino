b831868eb3c2e0739adcdf541c6a55f15ff4c5f4
anshu93 <anshuman.prasad93@gmail.com>
2014-01-27 13:50:47 -0500

    updated transmission file

--- Transmitter.ino
+++ Transmitter.ino
@@ -1,9 +1,15 @@
 //Define pins
-int thumb = A8;
-int index = A9;
-int middle = A10;
-int ring = A11;
-int pinky = A12;
+int thumb = 12;
+int index = 11;
+int middle = 10;
+int ring = 9;
+int pinky = 8;
+
+char arrThumb[4];
+char arrIndex[4];
+char arrMiddle[4];
+char arrRing[4];
+char arrPinky[4];
 
 void setup()
 {
@@ -13,33 +19,65 @@ void setup()
 
 void loop()
 {
-  int valThumb = analogRead(thumb);
-  int valIndex = analogRead(index);
-  int valMiddle = analogRead(middle);
-  int valRing = analogRead(ring);
-  int valPinky = analogRead(pinky);
   
-  int posThumb = map(analogRead(thumb), 700, 450, 0, 9);
+  int posThumb = map(analogRead(thumb), 520, 350, 0, 9);
   posThumb = constrain(posThumb, 0, 9);
+  // Creating the character array for the thumb
+  
+  arrThumb[0] = 'T';
+  arrThumb[1] = posThumb +'0';
+  arrThumb[2] = '.';
+ // Serial.println(thumb);
+  //delay(500);
+  
   
   int posIndex = map(analogRead(index), 550, 250, 0, 9);
   posIndex = constrain(posIndex, 0, 9);
+  // Creating the character array for the index finger
+
+  arrIndex[0] = 'I';
+  arrIndex[1] = posIndex +'0';
+  arrIndex[2] = '.';
+ // Serial.println(index);
+ // delay(500);
   
   int posMiddle = map(analogRead(middle), 560, 270, 0, 9);
   posMiddle = constrain(posMiddle, 0, 9);
- 
+  // Creating the character array for the middle finger
+
+  arrMiddle[0] = 'M';
+  arrMiddle[1] = posMiddle +'0';
+  arrMiddle[2] = '.';
+ // Serial.println(middle);
+ // delay(500);
+  
+  
   int posRing = map(analogRead(ring), 640, 270, 0, 9);
   posRing = constrain(posRing, 0, 9);
+  // Creating the character array for the ring finger
+
+  arrRing[0] = 'R';
+  arrRing[1] = posRing +'0';
+  arrRing[2] = '.';
+  //Serial.println(ring);
+  //delay(500);
   
   int posPinky = map(analogRead(pinky), 570, 280, 0, 9);
   posPinky = constrain(posPinky, 0, 9);
+  // Creating the character array for the pinky
+ 
+  arrPinky[0] = 'P';
+  arrPinky[1] = posPinky +'0';
+  arrPinky[2] = '.';
+  //Serial.println(pinky);
   
-  Serial.println(posThumb);
-  Serial.println(posIndex);
-  Serial.println(posMiddle);
-  Serial.println(posRing);
-  Serial.println(posPinky);
+  delay(200);
   
-  delay(500);
+  
+ Serial.println(arrThumb);
+ Serial.println(arrIndex);
+ Serial.println(arrMiddle);
+ Serial.println(arrRing);
+ Serial.println(arrPinky);
 }
   
